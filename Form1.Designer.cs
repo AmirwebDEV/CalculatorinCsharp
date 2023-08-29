a380a5abda12da2e0059385b64e639fcd1bc6cfc
AmirwebDEV <amirmanadrian@gmail.com>
2023-08-29 14:35:57 +0330

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,274 @@
+﻿namespace Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.btnN1 = new System.Windows.Forms.Button();
+            this.btnN2 = new System.Windows.Forms.Button();
+            this.btnN3 = new System.Windows.Forms.Button();
+            this.btnN4 = new System.Windows.Forms.Button();
+            this.btnN5 = new System.Windows.Forms.Button();
+            this.btnN6 = new System.Windows.Forms.Button();
+            this.btnN7 = new System.Windows.Forms.Button();
+            this.btnN8 = new System.Windows.Forms.Button();
+            this.btnN9 = new System.Windows.Forms.Button();
+            this.btnN0 = new System.Windows.Forms.Button();
+            this.btnSum = new System.Windows.Forms.Button();
+            this.btnMines = new System.Windows.Forms.Button();
+            this.btnTimes = new System.Windows.Forms.Button();
+            this.btnDivide = new System.Windows.Forms.Button();
+            this.btnAnswer = new System.Windows.Forms.Button();
+            this.btnDot = new System.Windows.Forms.Button();
+            this.btnClean = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.BackColor = System.Drawing.Color.Linen;
+            resources.ApplyResources(this.txtInput, "txtInput");
+            this.txtInput.Name = "txtInput";
+            this.txtInput.ReadOnly = true;
+            // 
+            // btnN1
+            // 
+            this.btnN1.BackColor = System.Drawing.Color.Violet;
+            this.btnN1.Cursor = System.Windows.Forms.Cursors.Hand;
+            resources.ApplyResources(this.btnN1, "btnN1");
+            this.btnN1.Name = "btnN1";
+            this.btnN1.UseVisualStyleBackColor = false;
+            this.btnN1.Click += new System.EventHandler(this.btnN1_Click);
+            // 
+            // btnN2
+            // 
+            this.btnN2.BackColor = System.Drawing.Color.Violet;
+            this.btnN2.Cursor = System.Windows.Forms.Cursors.Hand;
+            resources.ApplyResources(this.btnN2, "btnN2");
+            this.btnN2.Name = "btnN2";
+            this.btnN2.UseVisualStyleBackColor = false;
+            this.btnN2.Click += new System.EventHandler(this.btnN2_Click);
+            // 
+            // btnN3
+            // 
+            this.btnN3.BackColor = System.Drawing.Color.Violet;
+            this.btnN3.Cursor = System.Windows.Forms.Cursors.Hand;
+            resources.ApplyResources(this.btnN3, "btnN3");
+            this.btnN3.Name = "btnN3";
+            this.btnN3.UseVisualStyleBackColor = false;
+            this.btnN3.Click += new System.EventHandler(this.btnN3_Click);
+            // 
+            // btnN4
+            // 
+            this.btnN4.BackColor = System.Drawing.Color.Violet;
+            this.btnN4.Cursor = System.Windows.Forms.Cursors.Hand;
+            resources.ApplyResources(this.btnN4, "btnN4");
+            this.btnN4.Name = "btnN4";
+            this.btnN4.UseVisualStyleBackColor = false;
+            this.btnN4.Click += new System.EventHandler(this.btnN4_Click);
+            // 
+            // btnN5
+            // 
+            this.btnN5.BackColor = System.Drawing.Color.Violet;
+            this.btnN5.Cursor = System.Windows.Forms.Cursors.Hand;
+            resources.ApplyResources(this.btnN5, "btnN5");
+            this.btnN5.Name = "btnN5";
+            this.btnN5.UseVisualStyleBackColor = false;
+            this.btnN5.Click += new System.EventHandler(this.btnN5_Click);
+            // 
+            // btnN6
+            // 
+            this.btnN6.BackColor = System.Drawing.Color.Violet;
+            this.btnN6.Cursor = System.Windows.Forms.Cursors.Hand;
+            resources.ApplyResources(this.btnN6, "btnN6");
+            this.btnN6.Name = "btnN6";
+            this.btnN6.UseVisualStyleBackColor = false;
+            this.btnN6.Click += new System.EventHandler(this.btnN6_Click);
+            // 
+            // btnN7
+            // 
+            this.btnN7.BackColor = System.Drawing.Color.Violet;
+            this.btnN7.Cursor = System.Windows.Forms.Cursors.Hand;
+            resources.ApplyResources(this.btnN7, "btnN7");
+            this.btnN7.Name = "btnN7";
+            this.btnN7.UseVisualStyleBackColor = false;
+            this.btnN7.Click += new System.EventHandler(this.btnN7_Click);
+            // 
+            // btnN8
+            // 
+            this.btnN8.BackColor = System.Drawing.Color.Violet;
+            this.btnN8.Cursor = System.Windows.Forms.Cursors.Hand;
+            resources.ApplyResources(this.btnN8, "btnN8");
+            this.btnN8.Name = "btnN8";
+            this.btnN8.UseVisualStyleBackColor = false;
+            this.btnN8.Click += new System.EventHandler(this.btnN8_Click);
+            // 
+            // btnN9
+            // 
+            this.btnN9.BackColor = System.Drawing.Color.Violet;
+            this.btnN9.Cursor = System.Windows.Forms.Cursors.Hand;
+            resources.ApplyResources(this.btnN9, "btnN9");
+            this.btnN9.Name = "btnN9";
+            this.btnN9.UseVisualStyleBackColor = false;
+            this.btnN9.Click += new System.EventHandler(this.btnN9_Click);
+            // 
+            // btnN0
+            // 
+            this.btnN0.BackColor = System.Drawing.Color.Violet;
+            this.btnN0.Cursor = System.Windows.Forms.Cursors.Hand;
+            resources.ApplyResources(this.btnN0, "btnN0");
+            this.btnN0.Name = "btnN0";
+            this.btnN0.UseVisualStyleBackColor = false;
+            this.btnN0.Click += new System.EventHandler(this.btnN0_Click);
+            // 
+            // btnSum
+            // 
+            this.btnSum.BackColor = System.Drawing.Color.MediumPurple;
+            this.btnSum.Cursor = System.Windows.Forms.Cursors.Hand;
+            resources.ApplyResources(this.btnSum, "btnSum");
+            this.btnSum.Name = "btnSum";
+            this.btnSum.UseVisualStyleBackColor = false;
+            this.btnSum.Click += new System.EventHandler(this.btnAmalit_Click);
+            // 
+            // btnMines
+            // 
+            this.btnMines.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.btnMines.Cursor = System.Windows.Forms.Cursors.Hand;
+            resources.ApplyResources(this.btnMines, "btnMines");
+            this.btnMines.Name = "btnMines";
+            this.btnMines.UseVisualStyleBackColor = false;
+            this.btnMines.Click += new System.EventHandler(this.btnAmalit_Click);
+            // 
+            // btnTimes
+            // 
+            this.btnTimes.BackColor = System.Drawing.Color.MediumPurple;
+            this.btnTimes.Cursor = System.Windows.Forms.Cursors.Hand;
+            resources.ApplyResources(this.btnTimes, "btnTimes");
+            this.btnTimes.Name = "btnTimes";
+            this.btnTimes.UseVisualStyleBackColor = false;
+            this.btnTimes.Click += new System.EventHandler(this.btnAmalit_Click);
+            // 
+            // btnDivide
+            // 
+            this.btnDivide.BackColor = System.Drawing.Color.SlateBlue;
+            this.btnDivide.Cursor = System.Windows.Forms.Cursors.Hand;
+            resources.ApplyResources(this.btnDivide, "btnDivide");
+            this.btnDivide.Name = "btnDivide";
+            this.btnDivide.UseVisualStyleBackColor = false;
+            this.btnDivide.Click += new System.EventHandler(this.btnAmalit_Click);
+            // 
+            // btnAnswer
+            // 
+            this.btnAnswer.BackColor = System.Drawing.Color.LightPink;
+            this.btnAnswer.Cursor = System.Windows.Forms.Cursors.Hand;
+            resources.ApplyResources(this.btnAnswer, "btnAnswer");
+            this.btnAnswer.Name = "btnAnswer";
+            this.btnAnswer.UseVisualStyleBackColor = false;
+            this.btnAnswer.Click += new System.EventHandler(this.btnAnswer_Click);
+            // 
+            // btnDot
+            // 
+            this.btnDot.BackColor = System.Drawing.Color.Violet;
+            resources.ApplyResources(this.btnDot, "btnDot");
+            this.btnDot.Name = "btnDot";
+            this.btnDot.UseVisualStyleBackColor = false;
+            this.btnDot.Click += new System.EventHandler(this.btnDot_Click);
+            // 
+            // btnClean
+            // 
+            this.btnClean.BackColor = System.Drawing.Color.Pink;
+            resources.ApplyResources(this.btnClean, "btnClean");
+            this.btnClean.Name = "btnClean";
+            this.btnClean.UseVisualStyleBackColor = false;
+            this.btnClean.Click += new System.EventHandler(this.btnClean_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Crimson;
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ControlBox = false;
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnClean);
+            this.Controls.Add(this.btnDot);
+            this.Controls.Add(this.btnAnswer);
+            this.Controls.Add(this.btnDivide);
+            this.Controls.Add(this.btnTimes);
+            this.Controls.Add(this.btnMines);
+            this.Controls.Add(this.btnSum);
+            this.Controls.Add(this.btnN0);
+            this.Controls.Add(this.btnN9);
+            this.Controls.Add(this.btnN6);
+            this.Controls.Add(this.btnN3);
+            this.Controls.Add(this.btnN8);
+            this.Controls.Add(this.btnN7);
+            this.Controls.Add(this.btnN5);
+            this.Controls.Add(this.btnN4);
+            this.Controls.Add(this.btnN2);
+            this.Controls.Add(this.btnN1);
+            this.Controls.Add(this.txtInput);
+            this.Name = "Form1";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Button btnN1;
+        private System.Windows.Forms.Button btnN2;
+        private System.Windows.Forms.Button btnN3;
+        private System.Windows.Forms.Button btnN4;
+        private System.Windows.Forms.Button btnN5;
+        private System.Windows.Forms.Button btnN6;
+        private System.Windows.Forms.Button btnN7;
+        private System.Windows.Forms.Button btnN8;
+        private System.Windows.Forms.Button btnN9;
+        private System.Windows.Forms.Button btnN0;
+        private System.Windows.Forms.Button btnSum;
+        private System.Windows.Forms.Button btnMines;
+        private System.Windows.Forms.Button btnTimes;
+        private System.Windows.Forms.Button btnDivide;
+        private System.Windows.Forms.Button btnAnswer;
+        private System.Windows.Forms.Button btnDot;
+        private System.Windows.Forms.Button btnClean;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,246 @@
+﻿using System;
+
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        string[] strNum = new string[16] { "", "", "" ,"", "", "", "", "", "", "", "", "", "", "", "", "" };
+        double[] dblNum = new double[16] {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+        double resault = 0.0D;
+        string amaliat;
+        double FirstNumD = 0.0D;
+        double SecNumD = 0.0D;
+        double semiRes = 0.0D;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+
+        private void btnN1_Click(object sender, EventArgs e)
+        {
+            txtInput.Text += 1.ToString();
+
+        }
+
+        private void btnN2_Click(object sender, EventArgs e)
+        {
+            txtInput.Text += 2.ToString();
+
+        }
+
+        private void btnN3_Click(object sender, EventArgs e)
+        {
+            txtInput.Text += 3.ToString();
+
+        }
+
+        private void btnN4_Click(object sender, EventArgs e)
+        {
+            txtInput.Text += 4.ToString();
+
+        }
+
+        private void btnN5_Click(object sender, EventArgs e)
+        {
+            txtInput.Text += 5.ToString();
+
+        }
+
+        private void btnN6_Click(object sender, EventArgs e)
+        {
+            txtInput.Text += 6.ToString();
+
+        }
+
+        private void btnN7_Click(object sender, EventArgs e)
+        {
+            txtInput.Text += 7.ToString();
+
+        }
+
+        private void btnN8_Click(object sender, EventArgs e)
+        {
+            txtInput.Text += 8.ToString();
+
+        }
+
+        private void btnN9_Click(object sender, EventArgs e)
+        {
+            txtInput.Text += 9.ToString();
+
+        }
+
+        private void btnN0_Click(object sender, EventArgs e)
+        {
+            txtInput.Text += 0.ToString();
+        }
+
+        private void btnDot_Click(object sender, EventArgs e)
+        {
+            if (txtInput.Text == "")
+            {
+                txtInput.Text = "0.";
+            }
+            else if (txtInput.Text.Contains("."))
+            {
+
+            }
+            else
+            {
+                txtInput.Text += ".";
+            }
+        }
+
+        private void btnClean_Click(object sender, EventArgs e)
+        {
+            txtInput.Text = "";
+        }
+
+        //private void btnSum_Click(object sender, EventArgs e)
+        //{
+        //    int i = 0;
+
+        //    for (i = 0; i <= dblNum.Length; i++)
+        //    {
+        //        if (strNum[i] == "")
+        //        {
+        //            strNum[i] = txtInput.Text;
+        //            if (strNum[i] != "")
+        //            {
+        //                dblNum[i] = double.Parse(txtInput.Text);
+        //            }
+        //            break;
+        //        }
+        //    }
+        //    txtInput.Text = "";
+        //}
+
+        private void btnAnswer_Click(object sender, EventArgs e)
+        {
+            if (txtInput.Text != "")
+            {
+                switch (amaliat)
+                {
+                    case "+":
+                        SecNumD = Convert.ToDouble(txtInput.Text);
+                        semiRes += SecNumD;
+                        resault = semiRes;
+                        break;
+                    case "-":
+                        SecNumD = Convert.ToDouble(txtInput.Text);
+                        semiRes -= SecNumD;
+                        resault = semiRes;
+                        break;
+                    case "*":
+                        SecNumD = Convert.ToDouble(txtInput.Text);
+                        semiRes = (semiRes * SecNumD);
+                        resault = semiRes;
+                        break;
+                    case "/":
+                        SecNumD = Convert.ToDouble(txtInput.Text);
+                        semiRes = (semiRes / SecNumD);
+                        resault = semiRes;
+                        break;
+                }
+            }
+
+          
+            for (int i = 0; i < dblNum.Length; i++)
+            {
+                strNum[i] = "";
+                dblNum[i] = 0;
+            }
+            txtInput.Text = resault.ToString();
+            resault = 0;
+            semiRes = 0.0D;
+            amaliat = "";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnAmalit_Click(object sender, EventArgs e)
+        {
+       
+            FirstNumD = Convert.ToDouble(txtInput.Text);
+
+            Button opBtn = (Button)sender;
+            switch (opBtn.Text)
+            {
+                case "+":
+                    if (amaliat == "-")
+                    {
+                        semiRes -= FirstNumD;
+                        amaliat = "+";
+
+                        break;
+                    }
+                    semiRes += FirstNumD;
+                    amaliat = "+";
+                    break;
+                case "-":
+                    //for (int i = 0; i < 15; i++)
+                    //{
+                    //    resault -= dblNum[i];
+                    //}
+                    if (amaliat == "+")
+                    {
+                        semiRes += FirstNumD;
+                        amaliat = "-";
+                        break;
+                    }
+                    if (dblNum[0] == 0)
+                        semiRes += FirstNumD;
+                    if (dblNum[0] != 0)
+                    {
+                        semiRes -= FirstNumD;
+                        amaliat = "-";
+                        break;
+                    }
+                    amaliat = "-";
+                    break;
+                case "*":
+                    double MultiFnum = 0.0D;
+                    if (amaliat == "+" | amaliat == "-")
+                    {
+                        MultiFnum = Convert.ToDouble(txtInput.Text);
+
+                    }
+                    if (amaliat == "*")
+                    {
+                        semiRes = (semiRes * FirstNumD);
+                        break;
+                    }
+                    semiRes += FirstNumD;
+                    amaliat = "*";
+                    break;
+                case "/":
+                    if (amaliat == "/")
+                    {
+                        semiRes = (semiRes / FirstNumD);
+                        break;
+                    }
+                    semiRes += FirstNumD;
+                    amaliat = "/";
+                    break;
+
+            }
+            for (int i = 0; i < dblNum.Length; i++)
+            {
+                if (txtInput.Text != "")
+                {
+                    dblNum[i] = Convert.ToDouble(txtInput.Text);
+                    break;
+                }
+            }
+            txtInput.Text = "";
+
+        }
+    }
+}
